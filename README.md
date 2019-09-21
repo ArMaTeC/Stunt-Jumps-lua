cffb1fcec02c70d0aae7ed4f273b81d8b36107be
ArMaTeC <armatec0@gmail.com>
2019-09-21 09:05:50 +0100

    code clean up and getting ready for publish

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Stunt-Jumps-lua
+Stunt Jumps FiveM (In lua)
+All stock stunt jumps from GTA V in FiveM. 
+
+This resource adds all GTAV stunt jumps into your server.
+This is a clone of Vespura(TomGrobbe) resource recoded into lua
+
+Install
+Download the latest release from the releases page.
+Download into a new folder in your resources folder, you can call it whatever you like.
+Add start <name> to the server.cfg, making sure to replace <name> with the name you gave the new folder you just made.
+If you want to enable blips for all the stunt jumps, edit client.lua and change jumpsSetupBlips to true and jumpsSetupShowNearBlips to false
+If you want to enable blips for only the nearest stunt jumps, edit client.lua and change jumpsSetupBlips to true and jumpsSetupShowNearBlips to true
+
+Example and original work
+https://forum.fivem.net/t/stunt-jumps-all-default-stunt-jumps-from-gta-v-now-in-fivem/310208
+
+Developer notes
+There is a limit of 64 stunt jumps set by the game TomGrobbe created a list of 50 so there is 14 margin
+
+Would love some help with how to detect when a player compleates or fails a jump with and id of some sort so i can create the database side to store the jumps
+
+Changelog
+v1.0.0
+Initial release.
--- cl-stuntjumps/client.lua
+++ cl-stuntjumps/client.lua
@@ -1,95 +1,82 @@
-ESX = nil	
 local jumpsSetup = false
 local jumpsSetupBlips = true
+local jumpsSetupShowNearBlips = true
 local jumpsSetupBlipsTable = {}
 local jumpsSetupBlipsTable2 = {}
 local mindis = 300
 StatGetBool(GetHashKey("MP1_DEFAULT_STATS_SET"), jumpsSetup, -1);
-
 Citizen.CreateThread(function()
-	while ESX == nil do
-		TriggerEvent("esx:getSharedObject", function(sharedObject) ESX = sharedObject end)
-		Citizen.Wait(50)
-	end
-	while ESX.IsPlayerLoaded() == false do
-		Citizen.Wait(5)
-	end
-	PlayerData = ESX.GetPlayerData()
-	--JumpSet.TobGrobbe_1
-	local data = json.parse(JumpSet.TobGrobbe_1)
-	if type(data) == 'table' then
-		--print("TobGrobbe_1: ", ESX.DumpTable(data))
-		local jumpid = 0
-		for k, v in pairs(data) do
-			if k == "angledStuntJumps" then
-				print("=================angledStuntJumps=================")
-				for k, jump in pairs(v) do
-					--print(tostring(k) .. "   " .. tostring(v))
-					--print(ESX.DumpTable(v))
-					if not jumpsSetup then
-						table.insert(jumpsSetupBlipsTable, vector3(jump["jumpPos"]["start"]["X"],jump["jumpPos"]["start"]["Y"],jump["jumpPos"]["start"]["Z"]))
-						table.insert(jumpsSetupBlipsTable2, vector3(jump["landingPos"]["end"]["X"],jump["landingPos"]["end"]["Y"],jump["landingPos"]["end"]["Z"]))
-						DeleteStuntJump(jumpid)
-						AddStuntJumpAngled(
-							jump["jumpPos"]["start"]["X"],
-							jump["jumpPos"]["start"]["Y"],
-							jump["jumpPos"]["start"]["Z"],
-							jump["jumpPos"]["end"]["X"],
-							jump["jumpPos"]["end"]["Y"],
-							jump["jumpPos"]["end"]["Z"],
-							jump["jumpPos"]["radius"],
-							jump["landingPos"]["start"]["X"],
-							jump["landingPos"]["start"]["Y"],
-							jump["landingPos"]["start"]["Z"],
-							jump["landingPos"]["end"]["X"],
-							jump["landingPos"]["end"]["Y"],
-							jump["landingPos"]["end"]["Z"],
-							jump["landingPos"]["radius"],
-							jump["camPos"]["X"],
-							jump["camPos"]["Y"],
-							jump["camPos"]["Z"],
-							150,
-							0,
-							0
-						)
-						jumpid = jumpid + 1
+	for k, jumplist in pairs(JumpSet) do
+		local data = json.parse(jumplist)
+		if type(data) == 'table' then
+			local jumpid = 0
+			for k, v in pairs(data) do
+				if k == "angledStuntJumps" then
+					for k, jump in pairs(v) do
+						if not jumpsSetup then
+							table.insert(jumpsSetupBlipsTable, vector3(jump["jumpPos"]["start"]["X"],jump["jumpPos"]["start"]["Y"],jump["jumpPos"]["start"]["Z"]))
+							table.insert(jumpsSetupBlipsTable2, vector3(jump["landingPos"]["end"]["X"],jump["landingPos"]["end"]["Y"],jump["landingPos"]["end"]["Z"]))
+							DeleteStuntJump(jumpid)
+							AddStuntJumpAngled(
+								jump["jumpPos"]["start"]["X"],
+								jump["jumpPos"]["start"]["Y"],
+								jump["jumpPos"]["start"]["Z"],
+								jump["jumpPos"]["end"]["X"],
+								jump["jumpPos"]["end"]["Y"],
+								jump["jumpPos"]["end"]["Z"],
+								jump["jumpPos"]["radius"],
+								jump["landingPos"]["start"]["X"],
+								jump["landingPos"]["start"]["Y"],
+								jump["landingPos"]["start"]["Z"],
+								jump["landingPos"]["end"]["X"],
+								jump["landingPos"]["end"]["Y"],
+								jump["landingPos"]["end"]["Z"],
+								jump["landingPos"]["radius"],
+								jump["camPos"]["X"],
+								jump["camPos"]["Y"],
+								jump["camPos"]["Z"],
+								150,
+								0,
+								0
+							)
+							jumpid = jumpid + 1
+						end
 					end
-				end
-			elseif k == "normalStuntJumps" then
-				print("=================normalStuntJumps=================")
-				for k, jump in pairs(v) do
-					if not jumpsSetup then
-						table.insert(jumpsSetupBlipsTable, vector3(jump["jumpPos"]["start"]["X"],jump["jumpPos"]["start"]["Y"],jump["jumpPos"]["start"]["Z"]))
-						table.insert(jumpsSetupBlipsTable2, vector3(jump["landingPos"]["end"]["X"],jump["landingPos"]["end"]["Y"],jump["landingPos"]["end"]["Z"]))
-						DeleteStuntJump(jumpid)
-						AddStuntJump(
-							jump["jumpPos"]["start"]["X"],
-							jump["jumpPos"]["start"]["Y"],
-							jump["jumpPos"]["start"]["Z"],
-							jump["jumpPos"]["end"]["X"],
-							jump["jumpPos"]["end"]["Y"],
-							jump["jumpPos"]["end"]["Z"],
-							jump["landingPos"]["start"]["X"],
-							jump["landingPos"]["start"]["Y"],
-							jump["landingPos"]["start"]["Z"],
-							jump["landingPos"]["end"]["X"],
-							jump["landingPos"]["end"]["Y"],
-							jump["landingPos"]["end"]["Z"],
-							jump["camPos"]["X"],
-							jump["camPos"]["Y"],
-							jump["camPos"]["Z"],
-							150,
-							0,
-							0
-						)
-						jumpid = jumpid + 1
+				elseif k == "normalStuntJumps" then
+					for k, jump in pairs(v) do
+						if not jumpsSetup then
+							table.insert(jumpsSetupBlipsTable, vector3(jump["jumpPos"]["start"]["X"],jump["jumpPos"]["start"]["Y"],jump["jumpPos"]["start"]["Z"]))
+							table.insert(jumpsSetupBlipsTable2, vector3(jump["landingPos"]["end"]["X"],jump["landingPos"]["end"]["Y"],jump["landingPos"]["end"]["Z"]))
+							DeleteStuntJump(jumpid)
+							AddStuntJump(
+								jump["jumpPos"]["start"]["X"],
+								jump["jumpPos"]["start"]["Y"],
+								jump["jumpPos"]["start"]["Z"],
+								jump["jumpPos"]["end"]["X"],
+								jump["jumpPos"]["end"]["Y"],
+								jump["jumpPos"]["end"]["Z"],
+								jump["landingPos"]["start"]["X"],
+								jump["landingPos"]["start"]["Y"],
+								jump["landingPos"]["start"]["Z"],
+								jump["landingPos"]["end"]["X"],
+								jump["landingPos"]["end"]["Y"],
+								jump["landingPos"]["end"]["Z"],
+								jump["camPos"]["X"],
+								jump["camPos"]["Y"],
+								jump["camPos"]["Z"],
+								150,
+								0,
+								0
+							)
+							jumpid = jumpid + 1
+						end
 					end
 				end
 			end
 		end
 	end
+	jumpsSetup = true
 end)
-
 function CreateBlip(coords, id)
 	local blip = AddBlipForCoord(coords)
 	SetBlipSprite(blip, 490)
@@ -114,35 +101,44 @@ function CreateBlipLanding(coords, id)
 	EndTextCommandSetBlipName(blip)
 	return blip
 end
-
 Citizen.CreateThread(function()
 	local currentBlip = 0
 	local currentLandBlip = 0
-	while true do
-		Citizen.Wait(3000)
-		local coords = GetEntityCoords(PlayerPedId())
-		local closest = 100000000000
-		local closestCoords
-		local selectid = 0
-		for k,v in pairs(jumpsSetupBlipsTable) do
-			local dstcheck = GetDistanceBetweenCoords(coords, v)
-			if dstcheck < closest then
-				closest = dstcheck
-				closestCoords = v
-				selectid = k
+	if jumpsSetupShowNearBlips then
+		while jumpsSetupBlips do
+			Citizen.Wait(3000)
+			local coords = GetEntityCoords(PlayerPedId())
+			local closest = 100000000000
+			local closestCoords
+			local selectid = 0
+			for k,v in pairs(jumpsSetupBlipsTable) do
+				local dstcheck = GetDistanceBetweenCoords(coords, v)
+				if dstcheck < closest then
+					closest = dstcheck
+					closestCoords = v
+					selectid = k
+				end
+			end
+			if DoesBlipExist(currentBlip) then
+				RemoveBlip(currentBlip)
+			end
+			if DoesBlipExist(currentLandBlip) then
+				RemoveBlip(currentLandBlip)
+			end
+			if mindis >= closest then
+				currentBlip = CreateBlip(closestCoords, selectid)
+				currentLandBlip = CreateBlipLanding(jumpsSetupBlipsTable2[selectid], selectid)
 			end
 		end
-		if DoesBlipExist(currentBlip) then
-			RemoveBlip(currentBlip)
-		end
-		if DoesBlipExist(currentLandBlip) then
-			RemoveBlip(currentLandBlip)
-		end
-		--print(ESX.DumpTable(jumpsSetupBlipsTable2))
-		--print(selectid)
-		if mindis >= closest then
-			currentBlip = CreateBlip(closestCoords, selectid)
-			currentLandBlip = CreateBlipLanding(jumpsSetupBlipsTable2[selectid], selectid)
+	else
+		if jumpsSetupBlips then
+			while not jumpsSetup do
+				Citizen.Wait(0)
+			end
+			for k,v in pairs(jumpsSetupBlipsTable) do
+				currentBlip = CreateBlip(v, k)
+				currentLandBlip = CreateBlipLanding(jumpsSetupBlipsTable2[k], k)
+			end
 		end
 	end
 end)
